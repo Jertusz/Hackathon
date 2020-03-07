52b822cbbf5e7dfe94792c8408472a4601151f92
Jertusz <jendziu@gmail.com>
2020-03-07 20:46:24 +0100

    added ppl quantity

--- Hackathon/views.py
+++ Hackathon/views.py
@@ -33,7 +33,6 @@ def parse_recipe(unparsed_recipe):
     p_recipe = {}
     recka = unparsed_recipe['recipe']
     p_recipe['name'] = recka['label']
-    p_recipe['people'] = recka['yield']
     p_recipe['image'] = recka['image']
     p_recipe['source'] = recka['source']
     p_recipe['source_url'] = recka['url']

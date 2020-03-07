9409c0d28536edb7d61c200ef53b1b833ac1985c
Marcin Kloczkowski <marcin@kloczkowski.net>
2020-03-08 00:28:05 +0100

    added working landing page

--- Hackathon/forms.py
+++ Hackathon/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class queryForm(forms.Form):
-    q = forms.CharField(label="query", max_length=100)
+    q = forms.CharField(initial='', label="", max_length=100, widget = forms.HiddenInput())
